0b7b885404e7289da5b02fca41b71879cd8c6195
sisrikshab <54658027+sisrikshab@users.noreply.github.com>
2020-01-28 23:05:06 -0500

    Update deploy.sh

--- bqml/cloud_function/deploy.sh
+++ bqml/cloud_function/deploy.sh
@@ -3,4 +3,4 @@ echo "Thanks for using BQML modem deployment. Before starting, please ensure tha
 echo "---------------------------"
 read -p "Please enter your desired FUNCTION NAME (Make sure it is unique and only has alphanumeric characters and -, e.g. hello-world1): " function_name
 echo "~~~~~~~~ Creating function ~~~~~~~~~~"
-gcloud functions deploy $function_name --runtime python37 --memory 2GB --timeout 540s --trigger-http --entry-point trigger_workflow --project $gcp_project_id
+gcloud functions deploy $function_name --runtime python37 --memory 2GB --timeout 540s --trigger-http --entry-point trigger_workflow

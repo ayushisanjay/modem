a47472a5027785680d86c3cae5563812d77b2288
sisrikshab <54658027+sisrikshab@users.noreply.github.com>
2020-02-25 22:37:04 -0500

    Update deploy.sh

--- bqml/cloud_function/deploy.sh
+++ bqml/cloud_function/deploy.sh
@@ -5,5 +5,8 @@ read -p "Please enter your GCP PROJECT ID: " project_id
 echo "---------------------------"
 read -p "Please enter your desired FUNCTION NAME: " function_name
 echo "---------------------------"
+svc_account_email=`gcloud iam service-accounts list --filter="App Engine default service account" | awk 'FNR == 2{print $6}'` 
+sed_expr='s/SERVICE_ACCOUNT_EMAIL=""/SERVICE_ACCOUNT_EMAIL="'$svc_account_email'"/'
+echo `sed $sed_expr scheduler.sh` > scheduler.sh
 echo "~~~~~~~~ Creating function ~~~~~~~~~~"
 gcloud functions deploy $function_name --project $project_id --runtime python37 --memory 2GB --timeout 540s --trigger-http --entry-point trigger_workflow
